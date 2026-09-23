6aaab2525fa4473f45e9c90b0b1a9bd375fef11b
Henrik Uggla <henrik.uggla@kristianstad.se>
2026-09-23 13:45:20 +0200

    Fixa is_resource-kontroll för PHP 8.1+ pg-anslutningar i tableConfigs

--- finalfs/www/adm/functions/manage/tableConfigs.php
+++ finalfs/www/adm/functions/manage/tableConfigs.php
@@ -2,7 +2,8 @@
 
 	function tableConfigs($table, $configTablesOrDbh)
 	{
-		if (is_resource($configTablesOrDbh))
+		// pg_connect() returns a PgSql\Connection object since PHP 8.1, not a resource
+		if (is_resource($configTablesOrDbh) || $configTablesOrDbh instanceof \PgSql\Connection)
 		{
 			require("./constants/configSchema.php");
 			return all_from_table($configTablesOrDbh, $configSchema, $table);
